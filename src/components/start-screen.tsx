d9ebe2e10f0fd089027f6bc6555070ece2181553
ahqsa24 <ahqa24sadida@apps.ipb,ac,id>
2025-07-16 00:09:50 +0700

    add emoji

--- src/components/start-screen.tsx
+++ src/components/start-screen.tsx
@@ -7,7 +7,7 @@ export default function StartScreen({ onStart }: StartScreenProps) {
   return (
     <div className="text-center p-8">
       <h1 className="text-4xl font-bold text-gray-800 mb-6">
-        Birthday Game
+        🎁Birthday Game🎁
       </h1>
       <p className="text-gray-600 mb-8">Penasaran kan projek yang aku buat apa kemarin? nah ini dia waktu yang pas buat spill projek tersebut. Sejujurnya bingung mau dibuat gimana, tapi semoga kamu suka ya sama konsepnya. Love U, happy birthday yaa cantik!</p>
       <button
